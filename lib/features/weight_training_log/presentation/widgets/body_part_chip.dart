7512b3ad03c7f3624ba54978acc2de48fbdae221
adhan-857 <adhan.andika857@gmail.com>
2025-03-03 18:46:33 +0700

    [Green] Implement BodyPartChip widget to pass tests

---
+++ lib/features/weight_training_log/presentation/widgets/body_part_chip.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class BodyPartChip extends StatelessWidget {
+  final String category;
+  final bool isSelected;
+  final VoidCallback onTap;
+  final Color primaryGreen;
+
+  const BodyPartChip({
+    Key? key,
+    required this.category,
+    required this.isSelected,
+    required this.onTap,
+    required this.primaryGreen,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: isSelected ? primaryGreen : Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 3,
+              offset: Offset(0, 1),
+            ),
+          ],
+        ),
+        child: Text(
+          category,
+          style: TextStyle(
+            color: isSelected ? Colors.white : Colors.black54,
+            fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+}
