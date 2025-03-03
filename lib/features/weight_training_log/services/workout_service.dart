1cbd01392f2a35f9d58840ea0cad74fce5ccce3e
adhan-857 <adhan.andika857@gmail.com>
2025-03-03 17:20:51 +0700

    [Red] Add function signatures for workout calculations (tests should fail)

---
+++ lib/features/weight_training_log/services/workout_service.dart
@@ -0,0 +1,33 @@
+import 'package:pockeat/features/weight_training_log/domain/models/exercise.dart';
+
+const double defaultWeight = 75;
+const double k1 = 0.0001;
+const double k2 = 0.002;
+
+double calculateExerciseVolume(Exercise exercise) {
+  throw UnimplementedError();
+}
+
+double calculateTotalVolume(List<Exercise> exercises) {
+  throw UnimplementedError();
+}
+
+double calculateEstimatedCalories(List<Exercise> exercises) {
+  throw UnimplementedError();
+}
+
+double calculateExerciseCalories(Exercise exercise) {
+  throw UnimplementedError();
+}
+
+int calculateTotalSets(List<Exercise> exercises) {
+  throw UnimplementedError();
+}
+
+int calculateTotalReps(List<Exercise> exercises) {
+  throw UnimplementedError();
+}
+
+double calculateTotalDuration(List<Exercise> exercises) {
+  throw UnimplementedError();
+}
