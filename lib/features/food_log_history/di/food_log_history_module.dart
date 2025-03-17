da9150d9d2045dccb576ba78ae8fa7ae18a39900
isaui <isadestroyed17@gmail.com>
2025-03-17 13:18:30 +0700

    REFACTOR: fix duplicate food menu in food history page

--- lib/features/food_log_history/di/food_log_history_module.dart
+++ lib/features/food_log_history/di/food_log_history_module.dart
@@ -3,7 +3,6 @@ import 'package:pockeat/core/di/service_locator.dart';
 import 'package:pockeat/features/food_log_history/services/food_log_history_service.dart';
 import 'package:pockeat/features/food_log_history/services/food_log_history_service_impl.dart';
 import 'package:pockeat/features/food_scan_ai/domain/repositories/food_scan_repository.dart';
-import 'package:pockeat/features/food_text_input/domain/repositories/food_text_input_repository.dart';
 
 /// Registers all dependencies for the Food Log History feature
 class FoodLogHistoryModule {
@@ -15,7 +14,6 @@ class FoodLogHistoryModule {
     sl.registerLazySingleton<FoodLogHistoryService>(
       () => FoodLogHistoryServiceImpl(
         foodScanRepository: sl<FoodScanRepository>(),
-        foodTextInputRepository: sl<FoodTextInputRepository>(),
       ),
     );
   }
