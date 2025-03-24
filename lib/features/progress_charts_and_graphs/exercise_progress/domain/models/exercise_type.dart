2d8f4b59b3250aaea0a26284f2dc4783184f3d6f
adhan-857 <adhan.andika857@gmail.com>
2025-03-24 17:16:47 +0700

    [Green] Implement exercise_progress domain models

---
+++ lib/features/progress_charts_and_graphs/exercise_progress/domain/models/exercise_type.dart
@@ -0,0 +1,11 @@
+class ExerciseType {
+  final String name;
+  final int percentage;
+  final int colorValue;
+
+  ExerciseType({
+    required this.name,
+    required this.percentage,
+    required this.colorValue,
+  });
+}
