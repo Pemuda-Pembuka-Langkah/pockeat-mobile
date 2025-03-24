ede29f0b111d027528adc629dc8b22f76a7533fd
adhan-857 <adhan.andika857@gmail.com>
2025-03-24 14:35:10 +0700

    [Green] Implement analytics_insight domain models

---
+++ lib/features/progress_charts_and_graphs/analytics_insight/domain/models/analysis_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnalysisItem {
+  final String title;
+  final String value;
+  final String trend;
+  final Color color;
+
+  AnalysisItem({
+    required this.title,
+    required this.value,
+    required this.trend,
+    required this.color,
+  });
+}
