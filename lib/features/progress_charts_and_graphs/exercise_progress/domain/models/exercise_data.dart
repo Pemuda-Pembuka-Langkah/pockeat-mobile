2cdfd1fb7b5065c2dbf02545b38a399806b7cb0c
adhan-857 <adhan.andika857@gmail.com>
2025-04-07 20:00:11 +0700

    [GREEN] Implement exercise_progress smodels and repositories

--- lib/features/progress_charts_and_graphs/exercise_progress/domain/models/exercise_data.dart
+++ lib/features/progress_charts_and_graphs/exercise_progress/domain/models/exercise_data.dart
@@ -1,6 +1,18 @@
 class ExerciseData {
-  final String day;
-  final double calories;
-
-  ExerciseData(this.day, this.calories);
+  final dynamic date; // Can be String or DateTime
+  final double value;
+  
+  ExerciseData(this.date, this.value);
+  
+  // Helper to get formatted label based on date
+  String get label {
+    if (date is String) {
+      return date as String;
+    } else if (date is DateTime) {
+      // Format the DateTime to appropriate string
+      final dt = date as DateTime;
+      return '${dt.day}/${dt.month}';
+    }
+    return date.toString();
+  }
 }
--- lib/features/progress_charts_and_graphs/exercise_progress/domain/models/workout_item.dart
+++ lib/features/progress_charts_and_graphs/exercise_progress/domain/models/workout_item.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
+
 class WorkoutItem {
   final String title;
   final String type;
   final String stats;
   final String time;
   final int colorValue;
+  final IconData icon; // Add this new field
 
   WorkoutItem({
     required this.title,
@@ -11,5 +14,6 @@ class WorkoutItem {
     required this.stats,
     required this.time,
     required this.colorValue,
+    this.icon = Icons.fitness_center, // Default icon
   });
 }
