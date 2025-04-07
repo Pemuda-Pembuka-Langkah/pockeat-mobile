d0f5e9acc96a38aec406bfa088a4c7118dff3ab8
kimiagastya <kimiagastya@gmail.com>
2025-04-07 20:41:22 +0700

    [GREEN] Implement daily calorie stats repo, service, and widget

---
+++ lib/features/calorie_stats/di/calorie_stats_module.dart
@@ -0,0 +1,26 @@
+import 'package:get_it/get_it.dart';
+import 'package:pockeat/features/calorie_stats/domain/repositories/calorie_stats_repository.dart';
+import 'package:pockeat/features/calorie_stats/services/calorie_stats_service.dart';
+import 'package:pockeat/features/exercise_log_history/services/exercise_log_history_service.dart';
+import 'package:pockeat/features/food_log_history/services/food_log_history_service.dart';
+ // coverage:ignore-start
+class CalorieStatsModule {
+  static void register() {
+    final getIt = GetIt.instance;
+    
+    // Register repository
+    getIt.registerSingleton<CalorieStatsRepository>(
+      CalorieStatsRepositoryImpl(),
+    );
+    
+    // Register service
+    getIt.registerSingleton<CalorieStatsService>(
+      CalorieStatsServiceImpl(
+        repository: getIt<CalorieStatsRepository>(),
+        exerciseService: getIt<ExerciseLogHistoryService>(),
+        foodService: getIt<FoodLogHistoryService>(),
+      ),
+    );
+  }
+}
+ // coverage:ignore-end
