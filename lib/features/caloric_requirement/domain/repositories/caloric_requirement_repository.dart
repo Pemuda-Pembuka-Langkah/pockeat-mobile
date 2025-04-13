eca7edd2143ce2bd09fcecc7b18b79f2d2cd7c84
rxa15 <114054405+rxa15@users.noreply.github.com>
2025-04-13 13:41:02 +0700

    [GREEN] Implement user's daily caloric requirement repository

---
+++ lib/features/caloric_requirement/domain/repositories/caloric_requirement_repository.dart
@@ -0,0 +1,10 @@
+import '../models/caloric_requirement_model.dart';
+
+abstract class CaloricRequirementRepository {
+  Future<void> saveCaloricRequirement({
+    required String userId,
+    required CaloricRequirementModel result,
+  });
+
+  Future <CaloricRequirementModel?> getCaloricRequirement(String userId);
+}
---
+++ lib/features/caloric_requirement/domain/repositories/caloric_requirement_repository_impl.dart
@@ -0,0 +1,39 @@
+// lib/features/health_metrics/caloric_requirement/domain/repositories/caloric_requirement_repository_impl.dart
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pockeat/features/caloric_requirement/domain/models/caloric_requirement_model.dart';
+import 'caloric_requirement_repository.dart';
+
+class CaloricRequirementRepositoryImpl implements CaloricRequirementRepository {
+  final FirebaseFirestore firestore;
+
+  CaloricRequirementRepositoryImpl({FirebaseFirestore? firestore})
+      : firestore = firestore ?? FirebaseFirestore.instance;
+
+  @override
+  Future<void> saveCaloricRequirement({
+    required String userId,
+    required CaloricRequirementModel result,
+  }) async {
+    try {
+      await firestore
+          .collection('caloric_requirements')
+          .doc(userId)
+          .set(result.toMap(), SetOptions(merge: true));
+    } catch (e) {
+      throw Exception('Failed to save caloric requirement: $e');
+    }
+  }
+
+  @override
+  Future<CaloricRequirementModel?> getCaloricRequirement(String userId) async {
+    try {
+      final doc = await firestore.collection('caloric_requirements').doc(userId).get();
+      if (!doc.exists) return null;
+
+      return CaloricRequirementModel.fromMap(doc.data()!);
+    } catch (e) {
+      throw Exception('Failed to fetch caloric requirement: $e');
+    }
+  }
+}
