e8d5a67ab1f745c076c98216816eaee3c9fa4e49
rxa15 <114054405+rxa15@users.noreply.github.com>
2025-04-13 15:33:01 +0700

    [REFACTOR] add userId and timestamp fields in the Caloric Requirement model because I forgot earlier

--- lib/features/caloric_requirement/domain/models/caloric_requirement_model.dart
+++ lib/features/caloric_requirement/domain/models/caloric_requirement_model.dart
@@ -1,23 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class CaloricRequirementModel {
+  final String userId; // Firestore doc ID
   final double bmr;
   final double tdee;
+  final DateTime timestamp;
 
   CaloricRequirementModel({
+    required this.userId,
     required this.bmr,
     required this.tdee,
+    required this.timestamp,
   });
 
   Map<String, dynamic> toMap() {
     return {
+      'userId': userId,
       'bmr': bmr,
       'tdee': tdee,
+      'timestamp': timestamp.toIso8601String(),
     };
   }
 
-  factory CaloricRequirementModel.fromMap(Map<String, dynamic> map) {
+  factory CaloricRequirementModel.fromFirestore(DocumentSnapshot doc) {
+    final rawData = doc.data();
+    if (rawData == null) throw Exception("Document data is null");
+
+    final data = rawData as Map<String, dynamic>;
     return CaloricRequirementModel(
-      bmr: (map['bmr'] as num).toDouble(),
-      tdee: (map['tdee'] as num).toDouble(),
+      userId: doc.id,
+      bmr: (data['bmr'] as num).toDouble(),
+      tdee: (data['tdee'] as num).toDouble(),
+      timestamp: DateTime.parse(data['timestamp']),
     );
   }
 }
