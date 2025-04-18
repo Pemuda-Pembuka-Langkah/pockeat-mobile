d74f4135945c1e48e2479c684ecad56e587f2fd3
isaui <isadestroyed17@gmail.com>
2025-04-19 05:25:22 +0700

    [REFACTOR] refactor main.dart to include initialize of foodtrackingclientcontroller if !klsWeb

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ import 'package:pockeat/component/navigation.dart';
 import 'package:pockeat/features/food_scan_ai/presentation/screens/food_input_page.dart';
 import 'package:pockeat/features/api_scan/presentation/pages/ai_analysis_page.dart';
 import 'package:pockeat/core/di/service_locator.dart';
+import 'package:pockeat/features/home_screen_widget/controllers/food_tracking_client_controller.dart';
+import 'package:pockeat/features/home_screen_widget/di/home_widget_module.dart';
+import 'package:pockeat/features/authentication/domain/repositories/user_repository.dart';
 import 'package:pockeat/features/smart_exercise_log/domain/repositories/smart_exercise_log_repository.dart';
 import 'package:pockeat/features/cardio_log/presentation/screens/cardio_input_page.dart';
 import 'package:pockeat/features/exercise_log_history/services/exercise_log_history_service.dart';
@@ -66,6 +69,8 @@ import 'package:pockeat/features/authentication/presentation/screens/edit_profil
 import 'package:pockeat/features/authentication/domain/model/user_model.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+
 
 /// Initialize Instabug with token from .env file
 Future<bool> _initializeInstabug(String flavor) async {
@@ -126,6 +131,7 @@ void main() async {
 
   // Initialize notifications
   if (!kIsWeb) {
+    await getIt<FoodTrackingClientController>().initialize(navigatorKey);
     await getIt<NotificationService>().initialize();
   }
 
