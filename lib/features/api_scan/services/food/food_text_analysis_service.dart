0e68810af09c1a59d4ca2f715f86ff66d11d14e2
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2025-05-02 00:06:02 +0700

    chore: remove irrelevant prints

--- lib/features/api_scan/services/food/food_text_analysis_service.dart
+++ lib/features/api_scan/services/food/food_text_analysis_service.dart
@@ -52,11 +52,8 @@ class FoodTextAnalysisService {
         },
       );
 
-      //print('Response data: $responseData');
-
       final foodResult = FoodAnalysisResult.fromJson(responseData);
-      //print('INI MASUK APA KAGA SIH ANJING');
-      //print('Corrected food analysis: $foodResult');
+
       // Even if there's an error field, return a FoodAnalysisResult
       // The fromJson method or caller can handle the error appropriately
       return foodResult;
