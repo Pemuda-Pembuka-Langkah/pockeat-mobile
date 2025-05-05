f56a98613c1935af3672e7ec5d8bd38c3c89b9f6
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-05-05 12:24:25 +0700

    [GREEN] Implement integration with caloric requirements for pet companion

--- lib/features/pet_companion/domain/services/pet_service_impl.dart
+++ lib/features/pet_companion/domain/services/pet_service_impl.dart
@@ -1,4 +1,5 @@
 // Package imports:
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 
 // Project imports:
@@ -10,10 +11,12 @@ class PetServiceImpl implements PetService {
   final GetIt _getIt = GetIt.instance;
   late final FoodLogHistoryService foodLogHistoryService;
   late final CalorieStatsService calorieStatsService;
+  late final FirebaseFirestore firestore;
 
   PetServiceImpl() {
     foodLogHistoryService = _getIt<FoodLogHistoryService>();
     calorieStatsService = _getIt<CalorieStatsService>();
+    firestore = _getIt<FirebaseFirestore>();
   }
 
   @override
@@ -35,9 +38,10 @@ class PetServiceImpl implements PetService {
     final stats =
         await calorieStatsService.calculateStatsForDate(userId, DateTime.now());
 
-    const targetCalories = 2000; // static for now
+    final targetCalories =
+        await firestore.collection('caloric_requirements').doc(userId).get();
 
-    final percentage = stats.caloriesConsumed / targetCalories;
+    final percentage = stats.caloriesConsumed / targetCalories.data()!['tdee'];
 
     if (percentage > 0.75) {
       return 4;
