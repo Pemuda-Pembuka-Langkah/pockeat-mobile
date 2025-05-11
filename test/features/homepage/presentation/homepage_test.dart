733c2a5f5868b5626f04884bb2fe714d49f70886
kimiagastya <kimiagastya@gmail.com>
2025-05-11 14:43:37 +0700

    [RED] Removed duplicate import for homepage test

--- test/features/homepage/presentation/homepage_test.dart
+++ test/features/homepage/presentation/homepage_test.dart
@@ -20,7 +20,6 @@ import 'package:pockeat/features/homepage/presentation/screens/overview_section.
 import 'package:pockeat/features/homepage/presentation/screens/pet_homepage_section.dart';
 import 'package:pockeat/features/homepage/presentation/widgets/pet_companion_widget.dart';
 import 'package:pockeat/features/homepage/presentation/widgets/streak_counter_widget.dart';
-import 'package:pockeat/features/homepage/presentation/widgets/streak_counter_widget.dart';
 import 'package:pockeat/features/pet_companion/domain/services/pet_service.dart';
 import 'homepage_test.mocks.dart';
 
